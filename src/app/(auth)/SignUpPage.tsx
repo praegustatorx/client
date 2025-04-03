a217184478dd99a828d34b0852c70ded84cd2e4d
Radoslav Kiryazov <100125411+radoslavKiryazov@users.noreply.github.com>
2025-04-03 17:19:34 +0200

    Initial authentication

--- src/app/(auth)/SignUpPage.tsx
+++ src/app/(auth)/SignUpPage.tsx
@@ -1,12 +1,49 @@
 import { useState } from "react";
-import { TextInput, Button, Alert, StyleSheet } from "react-native";
+import { Button, Alert, StyleSheet } from "react-native";
+import { TextInput } from "../../components/Themed";
 import { View, Text } from "../../components/Themed";
 import { Link } from "expo-router";
+import axios from "axios";
+import { useMutation } from "react-query";
+
+interface RegisterCredentials {
+  email: string;
+  name: string;
+  password: string;
+}
+
 
 const SignUpPage = () => {
   const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+
+  const register = async (
+    credentials: RegisterCredentials
+  ): Promise<RegisterCredentials> => {
+    const response = await axios.post(
+      "http://10.154.252.20:8005/auth/register",
+      credentials
+    );
+    console.log(response);
+    return response.data;
+  };
+
+  const mutation = useMutation<RegisterCredentials, Error, RegisterCredentials>(
+    register,
+    {
+      onSuccess: (data) => {
+        // Show an alert with the modified email and password
+        Alert.alert(
+          "Signed up successfully",
+        );
+      },
+      onError: (error) => {
+        Alert.alert("Error", `Something went wrong: ${error.message}`);
+      },
+    }
+  );
 
   const handleSignUp = () => {
     if (!email || !password || !confirmPassword) {
@@ -17,7 +54,9 @@ const SignUpPage = () => {
       Alert.alert("Error", "Passwords do not match");
       return;
     }
-    Alert.alert("Sign Up Successful", `Email: ${email}, Password: ${password}`);
+    console.log('registering')
+    mutation.mutate({ email, name, password });
+
   };
 
   return (
@@ -31,6 +70,14 @@ const SignUpPage = () => {
         keyboardType="email-address"
         autoCapitalize="none"
       />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Name"
+        value={name}
+        onChangeText={setName}
+      />
+
       <TextInput
         style={styles.input}
         placeholder="Password"
@@ -70,8 +117,6 @@ const styles = StyleSheet.create({
     padding: 12,
     borderRadius: 8,
     marginBottom: 12,
-    borderWidth: 1,
-    color: "white",
   },
   link: {
     marginTop: 16,
