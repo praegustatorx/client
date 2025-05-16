0db8620a48283ea9e811151709ed5cbb4e706d04
Radoslav Kiryazov <100125411+radoslavKiryazov@users.noreply.github.com>
2025-05-16 14:50:21 +0200

    feat(wired up deletion)

---
+++ src/components/RecipeDetailsComponents/ActionButtons/Delete.tsx
@@ -0,0 +1,46 @@
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import RecipeActionButton from "../RecipeActionButton";
+import { useRecipeItem } from "@/src/providers/RecipeItemContext";
+import { useSession } from "@/src/providers/auth/AuthProvider";
+import { useDeleteRecipeFromCookbook } from "@/src/hooks/mutations/useDeleteRecipeFromCookbookMutation";
+import { router } from "expo-router";
+import { useNotificationToast } from "@/src/providers/ToastContext";
+import { useQueryClient } from "react-query";
+const Delete = () => {
+  const { selectedItem } = useRecipeItem();
+  const { user } = useSession();
+  const { showToast } = useNotificationToast();
+  const client = useQueryClient();
+  const { mutate, error } = useDeleteRecipeFromCookbook();
+
+  const handleDelete = () => {
+    mutate(
+      { recipeId: selectedItem!.id, userId: user!.email },
+      {
+        onSuccess: () => {
+          showToast({
+            message: "",
+            title: "Recipe removed cookbook",
+            duration: 2000,
+          });
+          client.invalidateQueries("cookbook");
+          router.replace("/(app)/(tabs)/two");
+        },
+        onError: (err) => {
+          showToast({
+            message: "Something went wrong. Pleasae try again later.",
+            title: "Failed",
+            duration: 2000,
+          });
+        },
+      }
+    );
+  };
+
+  return (
+    <RecipeActionButton action="Delete" onClick={handleDelete}>
+      <MaterialIcons name="delete-forever" size={24} color="red" />
+    </RecipeActionButton>
+  );
+};
+export default Delete;
